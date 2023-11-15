d810b580db15a28a969e8e203ce318dc054bdf81
haxxel1337 <65605299+haxxel1337@users.noreply.github.com>
2023-11-15 16:31:25 +0100

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,47 @@
+async function run() {
+  // A service worker must be registered in order to send notifications on iOS
+  const registration = await navigator.serviceWorker.register(
+    "serviceworker.js",
+    {
+      scope: "./",
+    }
+  );
+
+  const button = document.getElementById("subscribe");
+
+  const areNotificationsGranted = window.Notification.permission === "granted";
+  if (areNotificationsGranted) {
+    button.innerText = "Send Notification";
+
+    button.addEventListener("click", async () => {
+      await fetch("/send-notification");
+    });
+  } else {
+    button.addEventListener("click", async () => {
+      // Triggers popup to request access to send notifications
+      const result = await window.Notification.requestPermission();
+
+      // If the user rejects the permission result will be "denied"
+      if (result === "granted") {
+        const subscription = await registration.pushManager.subscribe({
+          // TODO: Replace with your public vapid key
+          applicationServerKey:
+            "BKs98_8WVyyTEObKiFdXK1h6iIfuzy8bTwIl9rg9KSLSyx0rYg3Pp6Xycmy-9fsVVgZrVrqfV-oJ7KADG8Af8Js",
+          userVisibleOnly: true,
+        });
+
+        await fetch("/save-subscription", {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(subscription),
+        });
+
+        window.location.reload();
+      }
+    });
+  }
+}
+
+run();
