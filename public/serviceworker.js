c119ebe502389d3242587c924ac176c0ef570d0d
haxxel1337 <65605299+haxxel1337@users.noreply.github.com>
2023-11-15 16:30:21 +0100

    Delete public directory

--- public/serviceworker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("push", async (event) => {
-  const { title, body } = await event.data.json();
-  console.log(title, body);
-  self.registration.showNotification(title, {
-    body,
-  });
-});
