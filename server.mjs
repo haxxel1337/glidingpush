dda8becefcaa0bdc2fa6b1aaa3489bad682c63eb
haxxel1337 <65605299+haxxel1337@users.noreply.github.com>
2023-11-15 16:33:34 +0100

    Add files via upload

---
+++ server.mjs
@@ -0,0 +1,39 @@
+import express from "express";
+import webpush from "web-push";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+let subscriptionData = null;
+
+webpush.setVapidDetails(
+  `mailto:${process.env.VAPID_MAILTO}`,
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
+)
+
+app.get('/send-notification', async (req, res) => {
+  try {
+    await webpush.sendNotification(subscriptionData, JSON.stringify({
+      title: "Gliding Eagle",
+      body: "Massa text från rapporten, kan va hur långt som helst. Undra hur många tecken som får plats."
+    }));
+    res.sendStatus(200);
+  } catch(err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+})
+
+app.post("/save-subscription", async (req, res) => {
+  subscriptionData = req.body;
+  res.sendStatus(200);
+});
+
+app.use(express.static("./public"));
+
+app.listen(process.env.PORT || 8000);
